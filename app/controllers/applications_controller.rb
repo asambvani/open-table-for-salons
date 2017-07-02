cb8ee9656b129625bcc45a71fc6daee689f43719
github username <github email address>
2017-07-02 15:07:28 -0400

    made front end changes

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < Sinatra::Base
   register Sinatra::Twitter::Bootstrap::Assets
 
   get '/' do
+    @salons = Salon.all
+    start_date = Time.now.to_datetime
+    end_date = start_date + 30
+    @date_range = (start_date...end_date).to_a
     erb :'/application/index'
   end
 
+  post '/availabilities' do
+    @date = "#{params[:date].month}-#{params[:date].day}-#{params[:date].year}"
+    @salon = params[:salon].name
+  end
 end
