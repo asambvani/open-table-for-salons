645d0d1d7940d2da55de0d913a045780fd37c4f7
asambvani <alexsambvani@gmail.com>
2017-07-03 13:09:47 -0400

    added update and delete functionality

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,25 @@
 class AppointmentController < ApplicationController
 
+  get '/appointments/:id' do
+    @appointment = Appointment.find(params[:id])
+    erb :'appointments/show'
+  end
+
+  patch '/appointments/:id' do
+    appointment = Appointment.find(params[:id])
+    appointment.update(date_time: params[:new_date_time])
+    redirect '/appointments/#{appointment.id}'
+  end
+
+  delete '/appointments/:id' do
+    appointment = Appointment.find(params[:id])
+    appointment.destroy
+    redirect '/'
+  end
+
   get '/appointments/:id/edit' do
-    
+    @appointment = Appointment.find(params[:id])
+    erb :'appointments/edit'
   end
 
 end
